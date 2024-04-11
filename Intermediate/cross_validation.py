34f86b58b98b7da5cfd9d4537f7f112c2ea0b475
Joseph Dulemba <jdulemba@Too-MacBook-Air.local>
2024-04-11 16:52:46 -0500

    Added small example testing different n_estimator values and then plotting the results.

--- Intermediate/cross_validation.py
+++ Intermediate/cross_validation.py
@@ -4,8 +4,8 @@ tic = time.time()
 """
 Construct the full pipeline in three steps.
 """
-
-import pandas as pd
+import numpy as np
+#import pandas as pd
 from pdb import set_trace
 import utils.Utilities as Utils
 from sklearn.ensemble import RandomForestRegressor
@@ -68,6 +68,53 @@ scores = -1 * cross_val_score(my_pipeline, X, y, cv = 5, scoring ="neg_mean_abso
 print("MAE scores:\n", scores)
 
 print(f"Average MAE score (across experiments): {scores.mean()}")
+
+
+
+## testing different parameter values by changing the n_estimators value and then plot
+def get_score(n_estimators):
+    """Return the average MAE over 5 CV folds of random forest model.
+    
+    Keyword argument:
+    n_estimators -- the number of trees in the forest
+    """
+    my_pipeline = Pipeline(
+        steps = [
+            ("preprocessor", SimpleImputer()),
+            ("model", RandomForestRegressor(n_estimators = n_estimators, random_state = 0))
+        ]
+    )
+    scores = -1 * cross_val_score(my_pipeline, X, y, cv = 5, scoring ="neg_mean_absolute_error")
+    return scores.mean()
+
+
+#set_trace()
+    # specify n_estimators values
+nEst_min, nEst_max, nEst_step = 800., 1500., 100.
+#nEst_min, nEst_max, nEst_step = 100., 1000., 100.
+nEst_array = np.arange(nEst_min, nEst_max + nEst_step, nEst_step, dtype=int)
+scores_array = np.array([[i, get_score(i)] for i in nEst_array])
+
+# find n_estimators value corresponding to minimum of MAE mean
+nEst_mae_min = scores_array[np.argmin(scores_array[:, 1]), 0]
+print(f"MAE min = {np.min(scores_array[:, 1]): .4f} occurs when using {nEst_mae_min} estimators")
+
+## plot results
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+fig.subplots_adjust(hspace=.07)
+ax.plot(list(scores_array[:, 0]), list(scores_array[:, 1]), **{"color" : "k"})
+ax.axvline(nEst_mae_min, color="k", linestyle="--")
+ax.autoscale()
+#ax.set_xlim(0., np.max(nEst_array))
+ax.set_ylabel("Mean Absolute Error Average")
+ax.set_xlabel("Number of Trees")
+ax.set_title("Results using Random Forest")
+fname = "results/RandomForest_nEstimators_Optimization"
+fig.savefig(fname, bbox_inches="tight")
+print(f"{fname} written")
+plt.close(fig)
+
 
 toc = time.time()
 print("Total runtime: %.1f" % (toc - tic))
