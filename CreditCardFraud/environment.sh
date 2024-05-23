ac6a4b0700f0bcfbac9b74312e9c06f7808eee82
Joseph Dulemba <jdulemba@Too-MacBook-Air.local>
2024-05-23 16:00:44 -0500

    Sets environmental variables.

---
+++ CreditCardFraud/environment.sh
@@ -0,0 +1,4 @@
+export PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
+export RESULTS_DIR=$PROJECT_DIR/results
+echo "Project working directory set to $PROJECT_DIR"
+echo "Results directory set to $RESULTS_DIR"
