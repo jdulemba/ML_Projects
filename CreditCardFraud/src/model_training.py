76e547b40e8d3b6d292a207c1304b713fe7fd8ce
Joseph Dulemba <jdulemba@Too-MacBook-Air.local>
2024-06-03 12:15:07 -0500

    Added ability to handle multiple types of optimizers, and also to run without using any optimizers.

--- CreditCardFraud/src/model_training.py
+++ CreditCardFraud/src/model_training.py
@@ -4,7 +4,7 @@ tic = time.time()
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("jobdir", help="The job directory name used as the output directory for this and the subsequent script's output.")
-parser.add_argument("--optimize", action="store_true", help="Output all print statements for debugging purposes.")
+parser.add_argument("--no_optimize", action="store_true", help="Ignore all parameter optimization algorithms and just train default models.")
 parser.add_argument("--debug", action="store_true", help="Output all print statements for debugging purposes.")
 parser.add_argument("--no_results", action="store_true", help="Suppress making output file.")
 args = parser.parse_args()
@@ -16,11 +16,6 @@ import numpy as np
 from copy import deepcopy
 
 import utils.model_options as model_opts
-if args.optimize:
-    import utils.model_optimization as optimize_model
-        ## plotting styles
-    import utils.plotting_scripts as plt_scripts
-
 from sklearn.base import clone # needed for 'initializing' models for each resampling method
 from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
 from sklearn.model_selection import cross_val_score
@@ -29,29 +24,37 @@ from sklearn.model_selection import cross_val_score
 resdir = os.path.join(os.environ["RESULTS_DIR"], args.jobdir)
 if not os.path.isdir(resdir):
     raise ValueError(f"{residr} could not be found. Please check if input is correct, otherwise run 'preprocessing.py'.")
-train_dir = os.path.join(resdir, "Training")
-if not os.path.isdir(train_dir): os.makedirs(train_dir)
-opt_dir = os.path.join(resdir, "ParameterOptimization")
-if not os.path.isdir(opt_dir): os.makedirs(opt_dir)
 
 # getting data
 inputfname = os.path.join(resdir, "processed_data.hdf5")
 try:
     with pd.HDFStore(inputfname) as storedata:
         features, target = storedata["training_features"].copy(), storedata["training_target"].copy()
-        if args.optimize:
-            test_features, test_target = storedata["testing_features"].copy(), storedata["testing_target"].copy()
         metadata = storedata.get_storer("training_features").attrs.metadata
+        if (metadata["Optimizers"] != ["Default"]) and (not args.no_optimize):
+        #if args.optimize:
+            test_features, test_target = storedata["testing_features"].copy(), storedata["testing_target"].copy()
     storedata.close()
 except:
     print(f"Could not get data from {inputfname}")
 
-scale = metadata["Scaler"]
-optimizer_algo = metadata["Optimizer"]
+scale = metadata["MetaData"]["Scaler"]
 
 # set random seeds for reproducibility
-rand_state = metadata["Random_State"]
+rand_state = metadata["MetaData"]["Random_State"]
 np.random.seed(rand_state)
+
+
+# get optimizer info
+optimizer_algos = metadata["Optimizers"]
+if args.no_optimize: optimizer_algos = ["Default"]
+if optimizer_algos != ["Default"]:
+    import utils.model_optimization as optimize_model
+        ## plotting styles
+    import utils.plotting_scripts as plt_scripts
+    opt_dir = os.path.join(resdir, "ParameterOptimization")
+    if not os.path.isdir(opt_dir): os.makedirs(opt_dir)
+
 
 
 # fit and evaluate models on training data
@@ -116,29 +119,31 @@ for pipe_name, pipeline in pipelines_dict.items():
         print(f"Number of normal transactions: {np.sum(y_res == 0)} ({np.sum(y_res == 0)/len(y_res)}%)")
         print(f"Number of fraud transactions: {np.sum(y_res == 1)} ({np.sum(y_res == 1)/len(y_res)}%)")
 
+    #set_trace()
+    tmp_classifiers_dict = {}
+    for optimizer_algo in optimizer_algos:
+        if optimizer_algo == "Default":
+            tmp_classifiers_dict.update({f"{key} {pipe_name} {optimizer_algo}" :  clone(val) for key, val in classifiers_options.items()})
+        else:
+            tmp_classifiers_dict.update({f"{key} {pipe_name} {optimizer_algo}" :  clone(val) for key, val in classifiers_options.items()})
+            #set_trace()
+            for key, val in classifiers_options.items():
+                clf = optimize_model.optimize_model(
+                    classifier=val, algo=optimizer_algo,
+                    data={"X_train" : X_res.copy(), "y_train" : y_res.copy(), "X_test" : test_features.copy(), "y_test" : test_target.copy()}
+                )
+                    # plot score for each hyperparameter combination
+                fig = plt_scripts.plot_gridsearch_results(clf, class_type=f"{key} {pipe_name} {optimizer_algo}")
+                fname = os.path.join(opt_dir, f"{key}_{pipe_name.replace(' ', '_')}_{scale}Scaler_ValidationCurves_ModelOptimized{optimizer_algo}")
+                fig.savefig(fname)
+                print(f"{fname} written")
+                fig.clear()
 
-    if args.optimize:
-            # create temp dict of classifiers to add to the results dict
-        tmp_classifiers_dict = {f"{key} {pipe_name} {optimizer_algo}" : None for key in classifiers_options.keys()}
-        #set_trace()
-        for key, val in classifiers_options.items():
-            clf = optimize_model.optimize_model(
-                classifier=val, algo=optimizer_algo,
-                data={"X_train" : X_res.copy(), "y_train" : y_res.copy(), "X_test" : test_features.copy(), "y_test" : test_target.copy()}
-            )
-                # plot score for each hyperparameter combination
-            fig = plt_scripts.plot_gridsearch_results(clf, class_type=f"{key} {pipe_name} {optimizer_algo}")
-            fname = os.path.join(opt_dir, f"{key}_{pipe_name.replace(' ', '_')}_{scale}Scaler_ValidationCurves_ModelOptimized{optimizer_algo}")
-            fig.savefig(fname)
-            print(f"{fname} written")
-            fig.clear()
+                tmp_classifiers_dict[f"{key} {pipe_name} {optimizer_algo}"] = clf.best_estimator_
 
-            tmp_classifiers_dict[f"{key} {pipe_name} {optimizer_algo}"] = clf.best_estimator_
-
-    else: # use default classifiers
-        tmp_classifiers_dict = {f"{key} {pipe_name} Default" : clone(val) for key, val in classifiers_options.items()}
     tmp_results_dict, tmp_classifiers_dict = fit_train_models(X=X_res, y=y_res, classifiers=tmp_classifiers_dict)
     classifiers_dict.update(tmp_classifiers_dict)
+
     results_dict.update(tmp_results_dict)
 
 
@@ -147,8 +152,7 @@ print(f"\n\n---------- Model Training Completed ----------\n\n{pd.DataFrame(resu
 
 # save results as pickle file
 if not args.no_results:
-    outfname = os.path.join(resdir, "TrainingResults_DefaultModels.pkl") if not args.optimize else\
-        os.path.join(resdir, f"TrainingResults_ModelOptimized{optimizer_algo}.pkl")
+    outfname = os.path.join(resdir, "TrainingResults.pkl")
 
     outdict = {
         "MetaData" : metadata,
