eb4ebca0c695774ad5bf5b36bad05e946456c98a
Joseph Dulemba <jdulemba@Too-MacBook-Air.local>
2024-06-03 12:22:06 -0500

    Created function which 'adds' information from 2 pickle files together, whether they're in the form of a list or a dictionary.

--- CreditCardFraud/utils/combine_results.py
+++ CreditCardFraud/utils/combine_results.py
@@ -19,6 +19,23 @@ import os
 import pickle
 from tqdm import tqdm
 
+def add(obj1, obj2):
+    if obj1 == obj2: return obj1
+    assert isinstance(obj1, type(obj2)), "Objects must be the same type in order to add them together!"
+    if isinstance(obj1, dict):
+        output = {}
+        for key in list(set(list(obj1.keys())+list(obj2.keys()))):
+            if obj1[key] == obj2[key]:
+                output[key] = obj1[key]
+            else:
+                output[key] = add(obj1[key], obj2[key])
+    elif isinstance(obj1, list):
+        output = sorted(set(obj1 + obj2))
+    else:
+            raise ValueError("Only dictionary or list objects can be added with this function")
+
+    return output
+
 if not os.path.isfile(input_files[0]): raise ValueError(f"Could not find {input_files[0]}")
 output_dict = pickle.load(open(input_files[0], "rb"))
 
@@ -27,11 +44,13 @@ for idx in tqdm(range(1, len(input_files))):
         tmp_dict = pickle.load(open(input_files[idx], "rb"))
 
         for key in output_dict.keys():
-            if key == "MetaData": assert output_dict[key] == tmp_dict[key], "Only files using the same data setup (MetaData) should be combined right now."
-            if output_dict[key] != tmp_dict[key]: output_dict[key].update(tmp_dict[key])
+            if (key == "MetaData") and (output_dict[key] != tmp_dict[key]):
+                merged_dicts = add(tmp_dict[key], output_dict[key])
+                output_dict[key].update(merged_dicts)
+            else:
+                output_dict[key].update(tmp_dict[key])
     except:
         raise ValueError("File %s (number %i) could not be added" % (input_files[idx], idx))
-
 
 with open(outfname, "wb") as outfile:
     pickle.dump(output_dict, outfile)
