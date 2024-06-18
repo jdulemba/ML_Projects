77dafaad10171265b14454aaf48be0b923612bf2
Joseph Dulemba <jdulemba@Too-MacBook-Air.local>
2024-06-18 13:24:45 -0500

    File no longer needed.

--- CreditCardFraud/prep_train_test.sh
+++
@@ -1,4 +0,0 @@
-echo "Running preprocessing, model training, and model testing with output in results/$1"
-python src/preprocessing.py $1
-python src/model_training.py $1
-python src/model_testing.py $1
