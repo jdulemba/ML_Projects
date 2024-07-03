2dcbb2461c11bb1ac8dc14f6eb6844b8d472238f
Joseph Dulemba <jdulemba@Too-MacBook-Air.local>
2024-07-02 21:41:49 -0500

    Added default MLFLOW_EXPERIMENT_NAME variable.

--- CreditCardFraud/environment.sh
+++ CreditCardFraud/environment.sh
@@ -3,3 +3,4 @@ export RESULTS_DIR=$PROJECT_DIR/results
 echo "Project working directory set to $PROJECT_DIR"
 echo "Results directory set to $RESULTS_DIR"
 export MLFLOW_TRACKING_URI=http://127.0.0.1:8080
+export MLFLOW_EXPERIMENT_NAME="Test Experiment"
